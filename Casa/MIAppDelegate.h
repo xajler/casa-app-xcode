dc3839af84030db47b97b2acbb743c2cdcfed031
Kornelije Sajler <xajler@gmail.com>
2012-11-29 02:34:08 +0100

    Initial commit

---
+++ Casa/MIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MIAppDelegate.h
+//  Casa
+//
+//  Created by Kornelije Sajler on 28.11.2012.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Casa/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Casa
+//
+//  Created by Kornelije Sajler on 28.11.2012.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MIAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MIAppDelegate class]));
+    }
+}
